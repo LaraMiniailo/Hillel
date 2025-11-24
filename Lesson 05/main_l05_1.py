4cb547a83b1991779b69a5d59e1871f01ac57c3b
lminy <sofokl1965@gmail.com>
2025-11-24 20:23:32 +0200

    HW 5.1. Виправлення помилок.

--- Lesson 05/main_l05_1.py
+++ Lesson 05/main_l05_1.py
@@ -1,31 +1,61 @@
-import string
+# HW 5.1. Ім'я змінної
+
 import keyword
-# Список для перевірки
-names = ["_","__","___","x","get_value","get value","get!value","some_super_puper_value","Get_value","get_Value","getValue","3m","m3","assert","assert_exception"]
-# Пунктуація, яку забороняємо
-banned = set(string.punctuation) - {"_"}
+import string
 
-for name in names:
-    is_valid = True
+list_hw51 = ['_','__','___','x','get_value','get value','get!value','some_super_puper_value',
+             'Get_value','get_Value','getValue','3m','m3','assert','assert_exception']
 
-    if not  name:
-        is_valid =  False
+# Змінна з усіма знаками пунктуації
+punctuation_chars = string.punctuation
 
-    elif name in keyword.kwlist:
-        is_valid = False
+for name_chars in list_hw51:
+    ok = True
 
-    elif name[0].isdigit():
-        is_valid = False
+    # Перевіряємо на порожній рядок
+    if name_chars == "":
+        ok = False
 
-    elif name.count("_") > 1:
-        is_valid = False
+    # Перевіряємо name_chars у списку зареєстрованих слов
+    if ok:
+        for kw in keyword.kwlist:
+            if name_chars == kw:
+                ok = False
+                break
 
-    else:
-        for ch in name:
-            if ch.isupper() or ch.isspace() or ch in banned:
-                is_valid = False
+    # Перевіряємо, чи не починається name_chars з цифри
+    if ok:
+        ch0 = name_chars[0]
+        if '0' <= ch0 <= '9':
+            ok = False
 
-            print(name, "=>", is_valid)
-            break
+    # Перевіряємо символи (дозволені: [a-z], [0-9], "_"; без пробілів і великих літер)
+    if ok:
+        i = 0
+        while i < len(name_chars):
+            ch = name_chars[i]
+
+            # Перевірка на великі літери
+            if 'A' <= ch <= 'Z':
+                ok = False
+                break
+
+            # Перевірка на пробіл
+            if ch == ' ':
+                ok = False
+                break
+
+            # Перевірка на символи пунктуації (за виключенням "_")
+            if ch in punctuation_chars and ch != '_':
+                ok = False
+                break
+
+            # В Python дозволені лише латинські малі літери, цифри та "_"
+            if not (('a' <= ch <= 'z') or ('0' <= ch <= '9') or ch == '_'):
+                ok = False
+                break
+            i += 1
+
+    print("'" + name_chars + "'", "->", ok)
 
 
