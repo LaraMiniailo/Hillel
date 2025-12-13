865b2f50b9b7b31df85fbf1315759f00e12b3ba8
lminy <sofokl1965@gmail.com>
2025-12-13 13:19:32 +0200

    Lesson 11.3.

--- Lesson 11/main_l11_3.py
+++ Lesson 11/main_l11_3.py
@@ -1,31 +1,25 @@
+# HW 11.3. Перевірка на парність.
+# Варыант 1.
+def is_even(n):
+    return (n & 1) == 0
 
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+print('Варыант 1.')
+print(is_even(2494563894038**2))
+print(is_even(1056897**2))
+print(is_even(24945638940387**3))
+print('====================================================')
 
+def is_even(n: int) -> bool:
+    return not (n & 1)
+print('Варыант 2.')
+print(is_even(2494563894038**2))
+print(is_even(1056897**2))
+print(is_even(24945638940387**3))
+print('====================================================')
 
-def bubble_sort_visualization(data):
-    """
-    Візуалізація сортування бульбашкою.
-
-    :param data: Початкові дані для сортування та візуалізації.
-    """
-    n = len(data)
-
-    fig, ax = plt.subplots()
-    bars = ax.bar(range(n), data, align='edge')
-
-    def update(frame):
-        for i in range(n - frame - 1):
-            if data[i] > data[i + 1]:
-                data[i], data[i + 1] = data[i + 1], data[i]
-
-        for bar, h in zip(bars, data):
-            bar.set_height(h)
-
-    ani = animation.FuncAnimation(fig, update, frames=n - 1, repeat=False)
-    plt.show()
-
-
-# Приклад використання
-initial_data = [64, 34, 25, 12, 22, 11, 90]
-bubble_sort_visualization(initial_data)
+def is_even(n: int) -> bool:
+    return not bool(n & 1)
+print('Варыант 3.')
+print(is_even(2494563894038**2))
+print(is_even(1056897**2))
+print(is_even(24945638940387**3))
