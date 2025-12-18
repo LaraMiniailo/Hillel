cd2e55e0dc5fe27f16065918c377d67af4b33484
lminy <sofokl1965@gmail.com>
2025-12-18 20:37:11 +0200

    Lesson 13. HW 13.2.

---
+++ Lesson 13/main_l13_2.py
@@ -0,0 +1,77 @@
+# HW 13.2. Клас "Цифровий лічильник"
+
+class Counter:
+    def __init__(self, current=1, min_value=0, max_value=10):
+        self.current = current
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def set_current(self, start):
+        if start < self.min_value or start > self.max_value:
+            raise ValueError("Початкове значення повинно бути між мінімумом та максимумом")
+        self.current = start
+
+    def set_max(self, max_max):
+        if max_max < self.min_value:
+            raise ValueError("Максимум не може бути меншим за мінімум")
+        self.max_value = max_max
+        if self.current > self.max_value:
+            self.current = self.max_value
+
+    def set_min(self, min_min):
+        if min_min > self.max_value:
+            raise ValueError("Мінімум не може бути більшим за максимум")
+        self.min_value = min_min
+        if self.current < self.min_value:
+            self.current = self.min_value
+
+    def step_up(self):
+        if self.current < self.max_value:
+            self.current += 1
+        else:
+            raise ValueError("Достигнут максимум")
+
+    def step_down(self):
+        if self.current > self.min_value:
+            self.current -= 1
+        else:
+            raise ValueError("Достигнут минимум")
+
+    def get_current(self):
+        return self.current
+
+
+# --- Перевірка ---
+counter = Counter()
+counter.set_current(7)
+counter.step_up()
+counter.step_up()
+counter.step_up()
+assert counter.get_current() == 10, 'Test1'
+try:
+    counter.step_up()  # ValueError
+except ValueError as e:
+    print(e)  # Достигнут максимум
+assert counter.get_current() == 10, 'Test2'
+
+counter.set_min(7)
+counter.step_down()
+counter.step_down()
+counter.step_down()
+assert counter.get_current() == 7, 'Test3'
+try:
+    counter.step_down()  # ValueError
+except ValueError as e:
+    print(e)  # Достигнут минимум
+assert counter.get_current() == 7, 'Test4'
+
+# Вивести поточне значення
+print("Поточне значення лічильника:", counter.get_current())
+
+# Крок вгору
+counter.step_up()
+print("Після step_up:", counter.get_current())
+
+# Крок вниз
+counter.step_down()
+print("Після step_down:", counter.get_current())
